a0c910b65385d6a140f9b7fe226c7bf482813cd1
Fredrik S. Dalen <fsdalen@gmail.com>
2016-01-25 15:52:32 +0100

    Imports modules and creates a simple material

--- script.py
+++ script.py
@@ -1,1 +1,57 @@
-#Test
+#====================================================================#
+#						PRELIMINARIES								 #
+#====================================================================#
+
+
+from part import *
+from material import *
+from section import *
+from optimization import *
+from assembly import *
+from step import *
+from interaction import *
+from load import *
+from mesh import *
+from job import *
+from sketch import *
+from visualization import *
+from connectorBehavior import *
+
+
+from abaqus import *			#These statements make the basic Abaqus objects accessible to the script... 
+from abaqusConstants import *	#... as well as all the Symbolic Constants defined in the Abaqus Scripting Interface.
+import odbAccess        		# To make ODB-commands available to the script
+
+
+#This makes mouse clicks into physical coordinates
+session.journalOptions.setValues(replayGeometry=COORDINATE,recoverGeometry=COORDINATE)
+
+
+
+#================ Input ==================#
+modelName = "basic_frame"
+M = mdb.models[modelName]		#For simplicity
+
+
+#====================================================================#
+#						MATERIALS 									 #
+#====================================================================#
+
+#================ Input ==================#
+
+# Material 1
+mat1 = "Steel"	#Material name
+mat1Description = 'This is the description'
+mat1_dens = 8.05e-06	#Density
+mat1_E = 210000.0		#E-module
+mat1_v = 0.3			#Poisson
+mat1_yield = 355		#Yield stress
+
+
+
+#================ Steel ==================#
+M.Material(description=mat1Description, name=mat1)
+M.materials[mat1].Density(table=((mat1_dens, ), ))
+M.materials[mat1].Elastic(table=((mat_E, mat1_v), ))
+M.materials[mat1].Plastic(table=((mat1_yield, 0.0), ))
+
