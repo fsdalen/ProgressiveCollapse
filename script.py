9013820d88939674c9f72f482712ac022e87f687
Fredrik S. Dalen <fsdalen@gmail.com>
2016-01-26 17:31:50 +0100

    Assemble beams and columns

--- script.py
+++ script.py
@@ -74,13 +74,13 @@ M.materials[mat1].Plastic(table=((mat1_yield, 0.0), ))
 #Column
 part1 = "Column"
 sect1 = "HUP"
-col1_height = 200
+col1_height = 3000
 
 
 #Beam
 part2 = "Beam"
 sect2 = "HUP2"
-beam1_len = 200
+beam_len = 5000
 
 
 #================ Column ==================#
@@ -112,8 +112,8 @@ M.BeamSection(consistentMassMatrix=False, integration=
     profile='Profile-2', temperatureVar=LINEAR)
 
 #Create part
-M.ConstrainedSketch(name='__profile__', sheetSize=20.0)
-M.sketches['__profile__'].Line(point1=(0.0, 0.0), point2=(beam1_len, 0.0))
+M.ConstrainedSketch(name='__profile__', sheetSize=10000.0)
+M.sketches['__profile__'].Line(point1=(0.0, 0.0), point2=(beam_len, 0.0))
 M.Part(dimensionality=THREE_D, name=part2, type=DEFORMABLE_BODY)
 M.parts[part2].BaseWire(sketch=M.sketches['__profile__'])
 del M.sketches['__profile__']
@@ -128,4 +128,63 @@ M.parts[part2].SectionAssignment(region=(faces, ), sectionName=sect2)
 #						ASSEMBLY 									 #
 #====================================================================#
 
+#================ Input ==================#
+x = 5			#Nr of columns in x direction
+z = 4			#Nr of columns in z direction
+x_d = beam_len		#Size of bays in x direction
+z_d = beam_len		#Size of bays in z direction
+
+y = 3			#nr of stories
+
+
+
+#================ Columns ==================#
+M.rootAssembly.DatumCsysByDefault(CARTESIAN)  #Set coordinates to Cartesian
+
+
+#Letters go left to right (positive x)
+#Number top to bottom (positive z)
+
+alph = map(chr, range(97, 97+x))
+numb = map(str,range(1,z+1))
+etg = map(str,range(1,y+1))
+
+count=-1
+for a in alph:
+	count = count + 1
+	for n in numb:
+		for e in etg:
+			inst = part1 + "_" + a + n + "-" + e
+			M.rootAssembly.Instance(dependent=ON,		#import and name instance
+				name= inst,
+				part=M.parts[part1])
+			M.rootAssembly.translate(instanceList=(inst, ),	#Translate instance in x,y and z
+				vector=(x_d*count , col1_height*(int(e)-1), z_d*(int(n)-1)))
+		
+
+#================ Beams ==================#'
+
+#Beams in x (alpha) direction
+for a in range(len(alph)-1):
+	for n in range(len(numb)-0):
+		for e in range(len(etg)):
+			inst = part2+"_"+ alph[a]+numb[n] + "-" + alph[a+1]+numb[n] + "-"+etg[e]		
+			#import and name instance
+			M.rootAssembly.Instance(dependent=ON,name=inst, part=M.parts[part2])
+			M.rootAssembly.translate(instanceList=(inst, ),
+				vector=(x_d*a , col1_height*(e+1), z_d*n))
+
+#Beams in z (numb) direction
+for a in range(len(alph)-0):
+	for n in range(len(numb)-1):
+		for e in range(len(etg)):
+			inst = part2+"_"+ alph[a]+numb[n] + "-" + alph[a]+numb[n+1] + "-"+etg[e]
+			#import and name instance
+			M.rootAssembly.Instance(dependent=ON,name=inst, part=M.parts[part2])
+			#Rotate instance
+			M.rootAssembly.rotate(angle=-90.0, axisDirection=(
+				0.0,1.0, 0.0), axisPoint=(0.0, 0.0, 0.0), instanceList=(inst, ))
+			#Translate instance in x,y and z
+			M.rootAssembly.translate(instanceList=(inst, ),
+				vector=(x_d*a , col1_height*(e+1), z_d*n))
 
