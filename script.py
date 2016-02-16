019ee9c301f47e24522974217f00c03608c15b8e
Fredrik S. Dalen <fsdalen@gmail.com>
2016-02-16 16:20:36 +0100

    Clean up inputs and merge instances

--- script.py
+++ script.py
@@ -26,9 +26,6 @@ import odbAccess        		# To make ODB-commands available to the script
 #This makes mouse clicks into physical coordinates
 session.journalOptions.setValues(replayGeometry=COORDINATE,recoverGeometry=COORDINATE)
 
-
-
-#================ Input ==================#
 modelName = "basicFrame"
 mdb.Model(modelType=STANDARD_EXPLICIT, name=modelName) 	#Create a new model 
 M = mdb.models[modelName]								#For simplicity
@@ -41,13 +38,12 @@ if len(mdb.models.keys()) > 0:							#Deletes all other models
 
 
 
-
 #====================================================================#
-#						MATERIALS 									 #
+#							INPUTS									 #
 #====================================================================#
 
-#================ Input ==================#
 
+#================ Materials ==================#
 # Material 1
 mat1 = "Steel"		#Material name
 mat1_Description = 'This is the description'
@@ -55,7 +51,6 @@ mat1_dens = 8.05e-09	#Density
 mat1_E = 210000.0		#E-module
 mat1_v = 0.3			#Poisson
 mat1_yield = 355		#Yield stress
-
 
 # Material 2
 mat2 = "Concrete"	#Material name
@@ -66,26 +61,7 @@ mat2_v = 0.3			#Poisson
 mat2_yield = 35			#Yield stress
 
 
-
-#================ Steel ==================#
-M.Material(description=mat1_Description, name=mat1)
-M.materials[mat1].Density(table=((mat1_dens, ), ))
-M.materials[mat1].Elastic(table=((mat1_E, mat1_v), ))
-M.materials[mat1].Plastic(table=((mat1_yield, 0.0), ))
-
-
-#================ Concrete ==================#
-M.Material(description=mat2_Description, name=mat2)
-M.materials[mat2].Density(table=((mat2_dens, ), ))
-M.materials[mat2].Elastic(table=((mat2_E, mat2_v), ))
-M.materials[mat2].Plastic(table=((mat2_yield, 0.0), ))
-
-
-#====================================================================#
-#						PARTS	 									 #
-#====================================================================#
-
-#================ Input ==================#
+#================ Parts ==================#
 #Column
 part1 = "Column"
 sect1 = "HUP"
@@ -101,8 +77,69 @@ part3 = "Slab"
 sect3 = "Slab"
 deck_t = 100	#Thickness of slabs
 
-#================ Column ==================#
 
+#================ Assembly ==================#
+x = 2			#Nr of columns in x direction
+z = 2			#Nr of columns in z direction
+y = 1			#nr of stories
+x_d = beam_len		#Size of bays in x direction
+z_d = beam_len		#Size of bays in z direction
+
+
+#================ Mesh ==================#
+analysisType = STANDARD  #Could be STANDARD or EXPLICIT
+
+#Column
+seed1 = 50
+element1 = B31 #B31 or B32 for linear or quadratic
+
+#Beam
+seed2 = seed1
+element2 = element1 #B31 or B32 for linear or quadratic
+
+#Slab
+seed3 = seed1
+element3 = S4R #S4R or S8R for linear or quadratic (S8R is not available for Explicit)
+
+
+#================ Step ==================#
+stepName = "Static"			#Name of step
+
+static = 0					# 1 if static
+riks = 1					# 1 if Riks static
+nlg = ON					# Nonlinear geometry (ON/OFF)
+
+
+#================ Loads ==================#
+LL=-5
+
+
+
+
+#====================================================================#
+#						MATERIALS 									 #
+#====================================================================#
+
+#================ Steel ==================#
+M.Material(description=mat1_Description, name=mat1)
+M.materials[mat1].Density(table=((mat1_dens, ), ))
+M.materials[mat1].Elastic(table=((mat1_E, mat1_v), ))
+M.materials[mat1].Plastic(table=((mat1_yield, 0.0), ))
+
+#================ Concrete ==================#
+M.Material(description=mat2_Description, name=mat2)
+M.materials[mat2].Density(table=((mat2_dens, ), ))
+M.materials[mat2].Elastic(table=((mat2_E, mat2_v), ))
+M.materials[mat2].Plastic(table=((mat2_yield, 0.0), ))
+
+
+
+
+#====================================================================#
+#						PARTS	 									 #
+#====================================================================#
+
+#================ Column ==================#
 #Create Section and profile
 M.BoxProfile(a=100.0, b=100.0, name='Profile-1', t1=8.0, uniformThickness=ON)
 M.BeamSection(consistentMassMatrix=False, integration=
@@ -122,7 +159,6 @@ M.parts[part1].SectionAssignment(offset=0.0,
     edges=M.parts[part1].edges.findAt(((0.0, 0.0, 
     0.0), ), )), sectionName=sect1, thicknessAssignment=FROM_SECTION)
 
-
 #Assign beam orientation
 M.parts[part1].assignBeamSectionOrientation(method=
     N1_COSINES, n1=(0.0, 0.0, -1.0), region=Region(
@@ -135,8 +171,8 @@ M.parts[part1].Set(name='col-base', vertices=
 M.parts[part1].Set(name='col-top', vertices=
     M.parts[part1].vertices.findAt(((0.0, col1_height, 0.0),)))
 
-#================ Beam ==================#
 
+#================ Beam ==================#
 #Create Section and profile
 M.BoxProfile(a=50.0, b=50.0, name='Profile-2', t1=4.0, uniformThickness=ON)
 M.BeamSection(consistentMassMatrix=False, integration=
@@ -156,7 +192,6 @@ M.parts[part2].SectionAssignment(offset=0.0,
     edges=M.parts[part2].edges.findAt(((0.0, 0.0, 
     0.0), ), )), sectionName=sect2, thicknessAssignment=FROM_SECTION)
 
-
 #Assign beam orientation
 M.parts[part2].assignBeamSectionOrientation(method=
     N1_COSINES, n1=(0.0, 0.0, -1.0), region=Region(
@@ -164,14 +199,12 @@ M.parts[part2].assignBeamSectionOrientation(method=
 
 
 #================ Slab ==================#
-
 #Create Section
 M.HomogeneousShellSection(idealization=NO_IDEALIZATION, 
     integrationRule=SIMPSON, material=mat2, name=sect3, numIntPts=5, 
     poissonDefinition=DEFAULT, preIntegrate=OFF, temperature=GRADIENT, 
     thickness=deck_t, thicknessField='', thicknessModulus=None, thicknessType=
     UNIFORM, useDensity=OFF)
-
 
 #Create part
 M.ConstrainedSketch(name='__profile__', sheetSize= 10000.0)
@@ -192,51 +225,46 @@ M.parts[part3].SectionAssignment(offset=0.0,
 M.parts[part3].Surface(name='Surf', side2Faces=
     M.parts[part3].faces.findAt(((0.0, 0.0, 0.0), )))
 
+
+
+
 #====================================================================#
 #						ASSEMBLY 									 #
 #====================================================================#
 
-#================ Input ==================#
-x = 3			#Nr of columns in x direction
-z = 3			#Nr of columns in z direction
-x_d = beam_len		#Size of bays in x direction
-z_d = beam_len		#Size of bays in z direction
-
-y = 2			#nr of stories
-
-
-
-#================ Columns ==================#
 M.rootAssembly.DatumCsysByDefault(CARTESIAN)  #Set coordinates to Cartesian
-
-
 #Letters go left to right (positive x)
 #Number top to bottom (positive z)
-
 alph = map(chr, range(65, 65+x)) #Start at 97 for lower case letters
 numb = map(str,range(1,z+1))
 etg = map(str,range(1,y+1))
 
+#Lists of all instances
+columnList = []
+beamList = []
+slabList = []
+
+#================ Columns ==================#
 count=-1
 for a in alph:
 	count = count + 1
 	for n in numb:
 		for e in etg:
 			inst = part1 + "_" + a + n + "-" + e
+			columnList.append(inst)
 			M.rootAssembly.Instance(dependent=ON,		#import and name instance
 				name= inst,
 				part=M.parts[part1])
 			M.rootAssembly.translate(instanceList=(inst, ),	#Translate instance in x,y and z
 				vector=(x_d*count , col1_height*(int(e)-1), z_d*(int(n)-1)))
 
-
 #================ Beams ==================#
-
 #Beams in x (alpha) direction
 for a in range(len(alph)-1):
 	for n in range(len(numb)-0):
 		for e in range(len(etg)):
 			inst = part2+"_"+ alph[a]+numb[n] + "-" + alph[a+1]+numb[n] + "-"+etg[e]		
+			beamList.append(inst)
 			#import and name instance
 			M.rootAssembly.Instance(dependent=ON,name=inst, part=M.parts[part2])
 			M.rootAssembly.translate(instanceList=(inst, ),
@@ -247,6 +275,7 @@ for a in range(len(alph)-0):
 	for n in range(len(numb)-1):
 		for e in range(len(etg)):
 			inst = part2+"_"+ alph[a]+numb[n] + "-" + alph[a]+numb[n+1] + "-"+etg[e]
+			beamList.append(inst)
 			#import and name instance
 			M.rootAssembly.Instance(dependent=ON,name=inst, part=M.parts[part2])
 			#Rotate instance
@@ -258,11 +287,11 @@ for a in range(len(alph)-0):
 
 
 #================ Slabs ==================#
-
 for a in range(len(alph)-1):
 	for n in range(len(numb)-1):
 		for e in range(len(etg)):
 			inst = part3+"_"+ alph[a]+numb[n] + "-"+etg[e]
+			slabList.append(inst)
 			M.rootAssembly.Instance(dependent=ON,name=inst, part=M.parts[part3])
 			M.rootAssembly.rotate(angle=90.0, axisDirection=(
 							1.0,0.0, 0.0), axisPoint=(0.0, 0.0, 0.0), instanceList=(inst, ))
@@ -270,26 +299,11 @@ for a in range(len(alph)-1):
 							vector=(x_d*a,col1_height*(e+1),z_d*n))
 
 
+
+
 #====================================================================#
 #							Mesh 									 #
 #====================================================================#
-
-#================ Input ==================#
-analysisType = STANDARD  #Could be STANDARD or EXPLICIT
-
-#Column
-seed1 = 50
-element1 = B31 #B31 or B32 for linear or quadratic
-
-#Beam
-seed2 = seed1
-element2 = element1 #B31 or B32 for linear or quadratic
-
-#Slab
-seed3 = seed1
-element3 = S4R #S4R or S8R for linear or quadratic (S8R is not available for Explicit)
-
-
 
 #================ Column ==================#
 #Seed
@@ -329,73 +343,81 @@ M.parts[part3].setElementType(elemTypes=(ElemType(
 M.parts[part3].generateMesh()
 
 
-#====================================================================#
-#							STEP 									 #
-#====================================================================#
-
-#================ Input ==================#
-stepName = "Static"			#Name of step
-
-static = 0					# 1 if static
-riks = 1					# 1 if Riks static
-nlg = ON					# Nonlinear geometry (ON/OFF)
-
-#================ Create step ==================#
-if static:
-	M.StaticStep(description='description', 
-		initialInc=0.1, name=stepName, nlgeom=nlg, previous='Initial')
-elif riks:
-	M.StaticRiksStep(description='description', 
-		initialArcInc=0.01, name=stepName, nlgeom=nlg, previous='Initial')
 
 
 #====================================================================#
-#							Joints 									 #
+#				MERGE INSTANCES TO SINGLE PART						 #
 #====================================================================#
 
 
+instList = columnList + beamList + slabList
+inst = []
+for i in instList:
+	inst.append(M.rootAssembly.instances[i])
+	print i
+instTup = tuple(inst)
+
+M.rootAssembly.regenerate()
+M.rootAssembly.InstanceFromBooleanMerge(domain=MESH, 
+    instances=instTup, mergeNodes=
+    BOUNDARY_ONLY, name='Part-1', nodeMergingTolerance=1e-06, 
+    originalInstances=SUPPRESS)
 
 
-
-
-#====================================================================#
-#							BCs 									 #
-#====================================================================#
-
-#================ Input ==================#
-LL=-5
-
-
-#================ Loads ==================#
-#Gravity
-M.Gravity(comp2=-9800.0, createStepName=stepName, 
-    distributionType=UNIFORM, field='', name='Gravity')
-
-
-#LL
-for a in range(len(alph)-1):
-	for n in range(len(numb)-1):
-		for e in range(len(etg)):
-			inst = "Slab_" + alph[a]+numb[n]+"-"+etg[e]
-			M.SurfaceTraction(createStepName=stepName, 
-				directionVector=((0.0, 0.0, 0.0), (0.0, 1.0, 0.0)),
-				distributionType=UNIFORM, field='', follower=OFF,
-				localCsys=None, magnitude= LL, name="Slab_" + alph[a]+numb[n]+"-"+etg[e],
-				region= M.rootAssembly.instances[inst].surfaces['Surf'],
-				traction=GENERAL)
-
-
-
-
-
-#================ Column base =============#
-count=-1
-for a in alph:
-	count = count + 1
-	for n in numb:
-		set = part1 + "_" + a + n + "-" + "1.col-base"
-		M.DisplacementBC(amplitude=UNSET, createStepName=
-			stepName, distributionType=UNIFORM, fieldName='', fixed=OFF,
-			localCsys=None, name=set, region=
-			M.rootAssembly.sets[set], u1=0.0, u2=0.0, u3=0.0
-			, ur1=0.0, ur2=0.0, ur3=0.0)
+# 
+# 
+# #====================================================================#
+# #							STEP 									 #
+# #====================================================================#
+# 
+# #================ Create step ==================#
+# if static:
+# 	M.StaticStep(description='description', 
+# 		initialInc=0.1, name=stepName, nlgeom=nlg, previous='Initial')
+# elif riks:
+# 	M.StaticRiksStep(description='description', 
+# 		initialArcInc=0.01, name=stepName, nlgeom=nlg, previous='Initial')
+# 
+# 
+# #====================================================================#
+# #							Joints 									 #
+# #====================================================================#
+# 
+# 
+# 
+# 
+# 
+# 
+# #====================================================================#
+# #							BCs 									 #
+# #====================================================================#
+# 
+# #================ Loads ==================#
+# #Gravity
+# M.Gravity(comp2=-9800.0, createStepName=stepName, 
+#     distributionType=UNIFORM, field='', name='Gravity')
+# 
+# #LL
+# for a in range(len(alph)-1):
+# 	for n in range(len(numb)-1):
+# 		for e in range(len(etg)):
+# 			inst = "Slab_" + alph[a]+numb[n]+"-"+etg[e]
+# 			M.SurfaceTraction(createStepName=stepName, 
+# 				directionVector=((0.0, 0.0, 0.0), (0.0, 1.0, 0.0)),
+# 				distributionType=UNIFORM, field='', follower=OFF,
+# 				localCsys=None, magnitude= LL, name="Slab_" + alph[a]+numb[n]+"-"+etg[e],
+# 				region= M.rootAssembly.instances[inst].surfaces['Surf'],
+# 				traction=GENERAL)
+# 
+# 
+# #================ Column base =============#
+# count=-1
+# for a in alph:
+# 	count = count + 1
+# 	for n in numb:
+# 		set = part1 + "_" + a + n + "-" + "1.col-base"
+# 		M.DisplacementBC(amplitude=UNSET, createStepName=
+# 			stepName, distributionType=UNIFORM, fieldName='', fixed=OFF,
+# 			localCsys=None, name=set, region=
+# 			M.rootAssembly.sets[set], u1=0.0, u2=0.0, u3=0.0
+# 			, ur1=0.0, ur2=0.0, ur3=0.0)
