f1d8b5ac0e52386b5b14716c53c013ddd125a09e
Fredrik S. Dalen <fsdalen@gmail.com>
2016-04-14 12:59:00 +0200

    Fix misstax in damping
    -Mass proportional damping should be alpha, not beta
    -Also uptaded damping to 0.05 (from litterature)

--- script.py
+++ script.py
@@ -224,8 +224,8 @@ if 1:
 # M.materials[mat1].plastic.setValues(table=((355.0, 
     # 0.0), (2000.0, 20.0)))
 
-#Damping (almost random mass proportional damping)
-M.materials[mat1].Damping(beta=0.0031)
+#Damping (Mass proportional damping)
+M.materials[mat1].Damping(alpha=0.05)
 
 #================ Concrete ==================#
 M.Material(description=mat2_Description, name=mat2)
@@ -233,8 +233,8 @@ M.materials[mat2].Density(table=((mat2_dens, ), ))
 M.materials[mat2].Elastic(table=((mat2_E, mat2_v), ))
 M.materials[mat2].Plastic(table=((mat2_yield, 0.0), ))
 
-#Damping (almost random mass proportional damping)
-M.materials[mat2].Damping(beta=0.0031)
+#Damping (Mass proportional damping)
+M.materials[mat2].Damping(alpha=0.05)
 
 #================ Rebar Steel ==================#
 M.Material(description=mat3_Description, name=mat3)
@@ -246,8 +246,8 @@ M.materials[mat3].Plastic(table=((mat3_yield, 0.0), ))
 M.materials[mat3].plastic.setValues(table=((355.0, 
     0.0), (2000.0, 20.0)))
 
-#Damping (almost random mass proportional damping)
-M.materials[mat3].Damping(beta=0.0031)
+#Damping (Mass proportional damping)
+M.materials[mat3].Damping(alpha=0.05)
 
 
 
