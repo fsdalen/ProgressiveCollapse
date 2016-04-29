04760096bb444684a77ec559eebf78a3a76bcb28
Fredrik S. Dalen <fsdalen@gmail.com>
2016-04-29 13:54:10 +0200

    Create a shell model with a static analysis

--- lib/shell.py
+++ lib/shell.py
@@ -22,10 +22,372 @@ import animation
 
 
 import func
-#from caeModules import *
+
 
 # HUP 300x300
 # HEB 550 beam
+
+
+
+#==============================================================#
+#==============================================================#
+#                   Create geometry                            #
+#==============================================================#
+#==============================================================#
+
+
+
+def createShellmod(modelName, x, z, y, steel, concrete, rebarSteel, seed):
+
+	col_height = 4200.0
+	beam_len = 8100.0
+
+	M=mdb.models[modelName]
+
+
+
+
+	#=========== Sections  ============#
+	# HUP 300x300
+	# HEB 550 beam
+
+	M.HomogeneousShellSection(
+		name='10mm',
+		thickness=10.0,
+		idealization=NO_IDEALIZATION, 
+		integrationRule=SIMPSON, material=steel, numIntPts=5, 
+		poissonDefinition=DEFAULT, preIntegrate=OFF,
+		temperature=GRADIENT, thicknessField='',
+		thicknessModulus=None, thicknessType=UNIFORM, useDensity=OFF)
+
+	M.HomogeneousShellSection(
+		name='11mm',
+		thickness=11.0,
+
+		idealization=NO_IDEALIZATION, 
+	    integrationRule=SIMPSON, material=steel, numIntPts=5, 
+	    poissonDefinition=DEFAULT, preIntegrate=OFF,
+	    temperature=GRADIENT, thicknessField='',
+	    thicknessModulus=None, thicknessType=UNIFORM, useDensity=OFF)
+
+	M.HomogeneousShellSection(
+		name='19mm',
+		thickness=19.0,
+
+		idealization=NO_IDEALIZATION, 
+	    integrationRule=SIMPSON, material=steel, numIntPts=5, 
+	    poissonDefinition=DEFAULT, preIntegrate=OFF,
+	    temperature=GRADIENT, thicknessField='',
+	    thicknessModulus=None, thicknessType=UNIFORM, useDensity=OFF)
+
+	#Concrete section
+	M.HomogeneousShellSection(
+		name='SLAB', material=concrete, 
+		idealization=NO_IDEALIZATION, 
+	    integrationRule=SIMPSON, numIntPts=5, 
+	    poissonDefinition=DEFAULT, preIntegrate=OFF,
+	    temperature=GRADIENT, thickness=200.0, thicknessField='',
+	    thicknessModulus=None, thicknessType=UNIFORM, useDensity=OFF)
+
+	#Add rebars to section
+	rebarDim = 20.0			#mm^2 diameter
+	rebarArea = 3.1415*(rebarDim/2.0)**2		#mm^2
+	rebarSpacing = 120.0		#mm
+	rebarPosition = -80.0		#mm distance from center of section
+
+	M.sections['SLAB'].RebarLayers(layerTable=(
+	    LayerProperties(barArea=rebarArea, orientationAngle=0.0,
+	    barSpacing=rebarSpacing, layerPosition=rebarPosition,
+	    layerName='Layer 1', material=rebarSteel), ), 
+	    rebarSpacing=CONSTANT)	
+
+
+
+
+	#=========== Column part  ============#
+	dep=ON
+	b=300.0
+	s= M.ConstrainedSketch(name='__profile__', sheetSize=x*beam_len)
+	point1=(0.5*b , 0.5*b)
+	point2=(-0.5*b , -0.5*b)
+	s.rectangle(point1=point1, point2=point2)
+
+
+	M.Part(dimensionality=THREE_D, name='COLUMN', type=
+	    DEFORMABLE_BODY)
+	M.parts['COLUMN'].BaseShellExtrude(
+		depth=col_height*y+0.5*b,
+		sketch=s)
+	del s
+
+	#Assign section
+	faces =M.parts['COLUMN'].faces.findAt(
+		((-150.0, 0.0,  100.0), (0.0, -1.0, 0.0)),
+		((150.0, 0.0, 100.0), (0.0, -1.0, 0.0)),
+		((0.0, -150.0, 100.0), (0.0, -1.0, 0.0)),
+		((0.0, 150.0, 100.0), (0.0, -1.0, 0.0)), )
+
+	M.parts['COLUMN'].SectionAssignment(offset=0.0, offsetField='',
+		offsetType=MIDDLE_SURFACE, region=Region(faces=faces), 
+		sectionName='10mm', thicknessAssignment=FROM_SECTION)
+
+	#Create bottom set
+	M.parts['COLUMN'].Set(name='colBot',
+		edges=M.parts['COLUMN'].edges.findAt(
+		((   0.0,  150.0,  0.0),),
+		(( 150.0,    0.0,  0.0),),
+		((-150.0,    0.0,  0.0),),
+		((   0.0, -150.0,  0.0),), ))
+
+	#Create surface
+	M.parts['COLUMN'].Surface(name='column', side12Faces=
+	    M.parts['COLUMN'].faces.findAt(
+		(( 150.0,   50.0, 100.0), ),
+		((  50.0, -150.0, 100.0), ),
+		((-150.0,  -50.0, 100.0), ),
+		((-50.0,   150.0, 100.0), ), ))
+
+
+
+	#=========== Beam part  ============#
+	s = M.ConstrainedSketch(name='__profile__', sheetSize=
+	200.0)
+	s.Line(point1 = (0.0, -150.0), point2 = (0.0,150.0))
+	s.Line(point1 = (-150.0, 150.0), point2 = (150.0, 150.0))
+	s.Line(point1 = (-150.0, -150.0), point2 = (150.0, -150.0))
+	M.Part(dimensionality=THREE_D, name='BEAM', type=
+	DEFORMABLE_BODY)
+	M.parts['BEAM'].BaseShellExtrude(depth=beam_len-b, sketch=s)
+	del s
+
+	#Assign section
+	faces =M.parts['BEAM'].faces.findAt(
+		((-50.0, 150.0,  100.0), (0.0, -1.0, 0.0)),
+		((50.0, 150.0, 100.0), (0.0, -1.0, 0.0)),
+		((-50.0, -150.0, 100.0), (0.0, -1.0, 0.0)),
+		((50.0, -150.0, 100.0), (0.0, -1.0, 0.0)), )
+
+	M.parts['BEAM'].SectionAssignment(offset=0.0, offsetField='',
+		offsetType=MIDDLE_SURFACE, region=Region(faces=faces), 
+		sectionName='19mm', thicknessAssignment=FROM_SECTION)
+
+	faces =M.parts['BEAM'].faces.findAt(
+		((0.0, 10.0,  100.0), (0.0, -1.0, 0.0)), )
+
+	M.parts['BEAM'].SectionAssignment(offset=0.0, offsetField='',
+		offsetType=MIDDLE_SURFACE, region=Region(faces=faces), 
+		sectionName='11mm', thicknessAssignment=FROM_SECTION)
+
+	#Surface
+	M.parts['BEAM'].Surface(name='beam', side12Faces=
+	    M.parts['BEAM'].faces.findAt(
+		((  0.0,   50.0, 100.0), ),
+		((-50.0,  150.0, 100.0), ),
+		(( 50.0,  150.0, 100.0), ),
+		((-50.0, -150.0, 100.0), ),
+		(( 50.0, -150.0, 100.0), ), )
+	    )
+
+
+
+
+	#=========== Slab part  ============#		
+	#Create part
+	gap = seed
+	s = M.ConstrainedSketch(name='__profile__', sheetSize= 10000.0)
+	s.rectangle(point1=(0.0, 0.0), point2=(beam_len-b-2*gap, beam_len))
+
+	M.Part(dimensionality=THREE_D, name='SLAB', type=DEFORMABLE_BODY)
+	M.parts['SLAB'].BaseShell(sketch=s)
+	del s
+
+	#Assign section
+	M.parts['SLAB'].SectionAssignment(offset=0.0, 
+	    offsetField='', offsetType=MIDDLE_SURFACE, region=Region(
+	    faces=M.parts['SLAB'].faces.findAt(((0.0, 
+	    0.0, 0.0), ), )), sectionName='SLAB', 
+	    thicknessAssignment=FROM_SECTION)
+
+	#Assign Rebar Orientation
+	M.parts['SLAB'].assignRebarOrientation(
+	    additionalRotationType=ROTATION_NONE, axis=AXIS_1,
+	    fieldName='', localCsys=None, orientationType=GLOBAL,
+	    region=Region(faces=M.parts['SLAB'].faces.findAt(
+	    ((0.1, 0.1, 0.0), (0.0, 0.0, 1.0)), )))
+
+	#Slab surf top and bottom
+	M.parts['SLAB'].Surface(name='topSurf', side2Faces=
+	    M.parts['SLAB'].faces.findAt(((0.0, 0.0, 0.0), )))
+	M.parts['SLAB'].Surface(name='botSurf', side1Faces=
+	    M.parts['SLAB'].faces.findAt(((0.0, 0.0, 0.0), )))
+
+
+
+
+
+	#=========== Assembly  ============#
+	#Columns
+	colNr=0
+	for a in range(x):
+		for n in range(z):
+			colNr = colNr +1
+			inst = 'COLUMN-'+str(colNr)
+			M.rootAssembly.Instance(
+				name = inst,
+				dependent=dep,
+				part=M.parts['COLUMN'])
+			#Rotate
+			M.rootAssembly.rotate(angle=-90.0,
+				axisDirection=(1.0,0.0, 0.0),
+				axisPoint=(0.0, 0.0, 0.0),
+				instanceList=(inst, ))
+			#Translate instance in x,y and z
+			M.rootAssembly.translate(instanceList=(inst, ),	
+				vector=(a*beam_len , 0, n*beam_len))
+			
+
+	#Beams in x- direction
+	beamNr = 0
+	for a in range(x-1):
+		for n in range(z):
+			for etg in range(y):
+				beamNr = beamNr+1
+				inst = 'BEAM-'+str(beamNr)
+				M.rootAssembly.Instance(
+					name = inst,
+					dependent = dep,
+					part=M.parts['BEAM'])
+				#Rotate
+				M.rootAssembly.rotate(angle=90.0,
+					axisDirection=(0.0,1.0, 0.0),
+					axisPoint=(0.0, 0.0, 0.0),
+					instanceList=(inst, ))
+				#Translate instance in x,y and z
+				M.rootAssembly.translate(instanceList=(inst, ),	
+					vector=(b*0.5+(beam_len)*a,(etg+1)*col_height,n*beam_len))
+	
+	#Beams in z-direction
+	for a in range(x):
+		for n in range(z-1):
+			for etg in range(y):
+				beamNr = beamNr+1
+				inst = 'BEAM-'+str(beamNr)
+				M.rootAssembly.Instance(
+					name = inst,
+					dependent = dep,
+					part=M.parts['BEAM'])
+				#Translate instance in x,y and z
+				M.rootAssembly.translate(instanceList=(inst, ),	
+					vector=(beam_len*a,(etg+1)*col_height,b*0.5+n*beam_len))
+
+	#Slabs
+	slabNr = 0
+	for a in range(x-1):
+		for n in range(z-1):
+			for etg in range(y):
+				slabNr = slabNr+1
+				inst = 'SLAB-'+str(slabNr)
+				M.rootAssembly.Instance(
+					name = inst,
+					dependent = dep,
+					part = M.parts['SLAB'])
+				#Rotate
+				M.rootAssembly.rotate(angle =90,
+					axisDirection=(1.0, 0.0, 0.0),
+					axisPoint=(0.0,0.0,0.0),
+					instanceList=(inst,))
+				#Translate
+				M.rootAssembly.translate(instanceList=(inst,),
+					vector=
+					(0.5*b+gap+a*beam_len, 0.5*b+(etg+1)*col_height, n*beam_len))
+
+
+
+	#=========== Merge instances  ============#
+	instLst = []
+	for key in M.rootAssembly.instances.keys():
+		instLst.append(M.rootAssembly.instances[key])
+	instTup	= tuple(instLst)
+	M.rootAssembly.InstanceFromBooleanMerge(domain=GEOMETRY
+	    , instances=instTup, name='Part-1'
+	    , originalInstances=DELETE,
+	    keepIntersections=ON)
+
+
+
+	#=========== Mesh  ============#
+	seed = 150
+	M.parts['Part-1'].seedPart(deviationFactor=0.1, 
+	    minSizeFactor=0.1, size=seed)
+	M.parts['Part-1'].generateMesh()
+
+	#Write nr of elements to results file
+	nrElm = len(M.parts['Part-1'].elements)
+	with open('results.txt', 'a') as f:
+		f.write("%s	Elements: %s \n" %(modelName, nrElm))
+
+
+	#=========== BC  ============#
+	#Fix column feet
+	M.DisplacementBC(amplitude=UNSET, createStepName=
+		'Initial', distributionType=UNIFORM, fieldName='',
+		localCsys=None, name='BC-1', 
+		region=mdb.models['shellStatic'].rootAssembly.sets['Part-1-1.colBot'],
+		u1=SET, u2=SET, u3=SET, ur1=SET, ur2=SET, ur3=SET)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -93,7 +455,7 @@ def conWep(modelName, TNT, blastType, coordinates, stepName):
 
 #===========================================================#
 #===========================================================#
-#                   Simple blast                            #
+#                  SIMPLE MODEL                            #
 #===========================================================#
 #===========================================================#
 
@@ -129,9 +491,9 @@ def createSingleBeam(modelName, steel):
 
 	#Assign section
 	faces =M.parts['Part-1'].faces.findAt(((-145.0, 
-		-48.333333, 2666.666667), (-1.0, 0.0, 0.0)), ((-48.333333, 145.0, 
-		2666.666667), (0.0, 1.0, 0.0)), ((145.0, 48.333333, 2666.666667),
-		(1.0, 0.0, 0.0)), ((48.333333, -145.0, 2666.666667),
+		-48.333333, 0.0), (-1.0, 0.0, 0.0)), ((-48.333333, 145.0, 
+		0.0), (0.0, 1.0, 0.0)), ((145.0, 48.333333, 0.0),
+		(1.0, 0.0, 0.0)), ((48.333333, -145.0, 0.0),
 		(0.0, -1.0, 0.0)), )
 
 	M.parts['Part-1'].SectionAssignment(offset=0.0, offsetField=
@@ -159,7 +521,7 @@ def createSingleBeam(modelName, steel):
 
 
 	#=========== Assembly  ============#
-	M.rootAssembly.Instance(dependent=ON, name='Part-1-1',
+	M.rootAssembly.Instance(dependent=dep, name='Part-1-1',
 		part=M.parts['Part-1'])
 	M.rootAssembly.rotate(angle=-90.0,
 		axisDirection=(1.0, 0.0, 0.0), axisPoint=(0.0, 0.0, 0.0),
@@ -168,10 +530,10 @@ def createSingleBeam(modelName, steel):
 	#Create blast surface
 	M.rootAssembly.Surface(name='blastSurf', side1Faces=
 	    M.rootAssembly.instances['Part-1-1'].faces.findAt(((
-	    -145.0, 2666.666667, 48.333333), ),
-	    ((-48.333333, 2666.666667, -145.0), ), 
-	    ((145.0, 2666.666667, -48.333333), ),
-	    ((48.333333, 2666.666667, 145.0), ), 
+	    -145.0, 0.0, 48.333333), ),
+	    ((-48.333333, 0.0, -145.0), ), 
+	    ((145.0, 0.0, -48.333333), ),
+	    ((48.333333, 0.0, 145.0), ), 
 	    ))
 
 
---
+++ shell_static.py
@@ -0,0 +1,177 @@
+#Abaqus modules
+from abaqus import *
+from abaqusConstants import *
+
+
+#=======================================================#
+#=======================================================#
+#                   CONTROLS                            #
+#=======================================================#
+#=======================================================#
+
+
+mdbName        = 'shellStatic'
+cpus           = 1			#Number of CPU's
+monitor        = 1
+run            = 0
+
+
+#=========== Geometry  ============#
+#Size 	4x4  x10(5)
+x              = 2			#Nr of columns in x direction
+z              = 2			#Nr of columns in z direction
+y              = 1			#nr of stories
+
+
+#=========== Static analysis  ============#
+static_Type    = 'general' 	#'general' or 'riks'
+static_InInc   = 0.1		# Initial increment
+static_MinIncr = 1e-9
+static_maxInc  = 50 		#Maximum number of increments for static step
+
+
+#=========== General  ============#
+#Live load
+LL_kN_m        = -2.0	    #kN/m^2 (-2.0)
+
+#Mesh
+seed           = 150.0		#Global seed
+
+#Post
+defScale       = 1.0
+printFormat    = PNG 		#TIFF, PS, EPS, PNG, SVG
+fieldIntervals = 30
+animeFrameRate = 5
+
+
+
+#==========================================================#
+#==========================================================#
+#                   Perliminary                            #
+#==========================================================#
+#==========================================================#
+
+import lib.func as func
+import lib.shell as shell
+reload(func)
+reload(shell)
+
+modelName   = mdbName
+
+steel = 'DOMEX_S355'
+concrete = 'Concrete'
+rebarSteel = 'Rebar Steel'
+
+#Set up model with materials
+func.perliminary(monitor, modelName, steel, concrete, rebarSteel)
+
+M=mdb.models[modelName]
+
+
+
+
+#==========================================================#
+#==========================================================#
+#                   Build model                            #
+#==========================================================#
+#==========================================================#
+
+#Build geometry
+
+shell.createShellmod(modelName, x, z, y, steel, concrete, rebarSteel, seed)
+
+
+
+
+#================================================================#
+#================================================================#
+#                   STEP DEPENDENCIES                            #
+#================================================================#
+#================================================================#
+
+
+#=========== Step  ============#
+oldStep = 'Initial'
+stepName = 'static'
+if static_Type == 'general':
+	M.StaticStep(name=stepName, previous=oldStep, 
+		nlgeom=ON, initialInc=static_InInc, minInc=static_MinIncr,
+		maxNumInc=static_maxInc)
+elif static_Type == 'riks':
+	M.StaticRiksStep(name=stepName, previous=oldStep, 
+		nlgeom=ON, initialArcInc=static_InInc, minArcInc=static_MinIncr,
+		maxNumInc=static_maxInc, maxLPF=1.0)
+
+
+
+#=========== Loads  ============#
+# Gravity
+M.Gravity(comp2=-9800.0, createStepName=stepName, 
+	distributionType=UNIFORM, field='', name='Gravity')
+
+#LL
+LL=LL_kN_m * 1.0e-3   #N/mm^2
+
+M.SurfaceTraction(createStepName=stepName, 
+	directionVector=((0.0, 0.0, 0.0), (0.0, 1.0, 0.0)),
+	distributionType=UNIFORM, field='', follower=OFF,
+	localCsys=None, magnitude= LL,
+	name="LL",
+	region=M.rootAssembly.instances['Part-1-1'].surfaces['topSurf'],
+	traction=GENERAL)
+
+M.rootAssembly.regenerate()
+
+
+#=====================================================#
+#=====================================================#
+#                   Output                            #
+#=====================================================#
+#=====================================================#
+
+
+#Delete default history output
+del M.historyOutputRequests['H-Output-1']
+
+
+
+#===========================================================#
+#===========================================================#
+#                   Save and run                            #
+#===========================================================#
+#===========================================================#
+M.rootAssembly.regenerate()
+
+#Save model
+mdb.saveAs(pathName = mdbName + '.cae')
+
+#Create job
+mdb.Job(model=modelName, name=modelName, numCpus=cpus)
+
+#Run job
+if run:
+	func.runJob(modelName)
+	#Write CPU time to file
+	func.readMsgFile(modelName, 'results.txt')
+
+
+
+#===================================================#
+#===================================================#
+#                   Post                            #
+#===================================================#
+#===================================================#
+
+	print 'Post processing...'
+
+	#Clear plots
+	for plot in session.xyPlots.keys():
+		del session.xyPlots[plot]
+
+	#=========== Contour  ============#
+	func.countourPrint(modelName, defScale, printFormat)
+
+	#=========== Animation  ============#
+	func.animate(modelName, defScale, frameRate= animeFrameRate)
+	
+	print '   done'
